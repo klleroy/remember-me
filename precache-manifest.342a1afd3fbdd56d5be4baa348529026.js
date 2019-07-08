c3228c7591f560d81be407c6b86de2fe9c0fc026
KevinL <mastahkevin@gmail.com>
2019-07-07 22:40:12 -0700

    Updates

---
+++ precache-manifest.342a1afd3fbdd56d5be4baa348529026.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50c14605933c3b1543b2f9aff8a8f1f",
+    "url": "/remember-me/index.html"
+  },
+  {
+    "revision": "36d27a461f8033a5873d",
+    "url": "/remember-me/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/remember-me/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "36d27a461f8033a5873d",
+    "url": "/remember-me/static/js/main.cd4df591.chunk.js"
+  },
+  {
+    "revision": "905feb6067e0c61017de",
+    "url": "/remember-me/static/js/runtime~main.d3cf414d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/remember-me/static/media/logo.5d5d9eef.svg"
+  }
+]);
