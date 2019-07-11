5a848cf865c3cc43188905b4f0c00e7884e7279f
Kevin LeRoy (HCL America Inc) <v-keler@microsoft.com>
2019-07-11 15:11:37 -0700

    Continued buildout of the Application. Creating imports, Class, and shuffle function. Added icon.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,48 @@
 import React, { Component } from 'react';
 import FriendCard from './components/FriendCard/index';
 import Nav from './components/Nav/index';
+import Title from './components/Title/index'
 import Wrapper from './components/Wrapper/index';
 import Row from './Row';
+import Column from './Column';
+import friends from './friends.json';
+import './App.css';
 
+// Shuffle Friends
+shuffleFriends = (arr) => {
+    for (let i = arr.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// create Class
+class App extends Component {
+    // set state
+    state = {
+        friends,
+        currentScore: 0,
+        topScore: 0,
+        trueFalse: '',
+        clicked: []
+    }
 }
 
+// handle click
+
+
+// handle score increment
+
+
+// handle game reset
+
+
+// handle shuffle
+
+
+// render screen
+
+
+// export App
 export default App;
