d32fe56e89e9b32ec259aaf18852c3dbb3818df8
Kevin LeRoy (HCL America Inc) <v-keler@microsoft.com>
2019-07-11 14:17:20 -0700

    continuing to buildout App with jsx. Re-Added app.css and app.test.js as they were deleted accidentally.

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard/index";
+import Wrapper from "./components/Wrapper/index";
+import Title from "./components/Title/index";
+import friends from "./friends.json";
+
+class App extends Component {
+    // set state
+    state = {
+        friends,
+        currentScore: 0,
+        topScore: 0,
+        trueFalse: '',
+        clicked: []
+    }
+}
+
+// Click Handler
+
+
+// Increment Score
+
+
+// Reset Score
+
+
+// Shuffle Deck
+
+
+// Render Screen
+
+
+// export app
+exports default App;
