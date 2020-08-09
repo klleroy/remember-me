b261a0510d6edafe3d986b4b5dda3b552fdcef00
Kevin LeRoy <kevin@kevinleroy.me>
2020-08-08 17:28:15 -0700

    Updates

---
+++ precache-manifest.7525fc33d146b4db1f4b4608f3ca3632.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb2dc1e59c8140d9d4466754a804744",
+    "url": "/remember-me/index.html"
+  },
+  {
+    "revision": "4e0a37df082a0b1c3151",
+    "url": "/remember-me/static/css/main.ff14d6e4.chunk.css"
+  },
+  {
+    "revision": "9c120cf3e3a336f438eb",
+    "url": "/remember-me/static/js/2.b4fc5adf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/remember-me/static/js/2.b4fc5adf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e0a37df082a0b1c3151",
+    "url": "/remember-me/static/js/main.33be3419.chunk.js"
+  },
+  {
+    "revision": "66d92e7ddb97a3057647",
+    "url": "/remember-me/static/js/runtime-main.b87a6d53.js"
+  }
+]);
